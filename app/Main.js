a77d4a309a1b765d5c70974468b6c46608c463f2
omar <omasrt@gmail.com>
2019-07-22 17:13:07 -0500

    creates Input component for user input

--- app/Main.js
+++ app/Main.js
@@ -2,18 +2,34 @@ import React from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { primaryGradientArray } from "./utils/Colors";
+import { gradienStart, gradientEnd } from "./utils/Colors";
 
 import Header from "./components/Header";
+import Input from "./components/Input";
 
 const headerTitle = "To Do";
 
 export default class Main extends React.Component {
+  state = {
+    inputValue: ""
+  };
+
+  newInputValue = value => {
+    this.setState({
+      inputValue: value
+    });
+  };
+
   render() {
+    const { inputValue } = this.state;
     return (
       <LinearGradient colors={primaryGradientArray} style={styles.container}>
         <StatusBar barStyle="light-content" />
         <View style={styles.centered}>
           <Header title={headerTitle} />
+        </View>
+        <View style={styles.inputContainer}>
+          <Input inputValue={inputValue} onChangeText={this.newInputValue} />
         </View>
       </LinearGradient>
     );
@@ -26,5 +42,9 @@ const styles = StyleSheet.create({
   },
   centered: {
     alignItems: "center"
+  },
+  inputContainer: {
+    marginTop: 40,
+    paddingLeft: 15
   }
 });
